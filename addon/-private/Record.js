11c1ce007821ecfb2cc437b9102beedbc356e3f7
Offir Golan <offirgolan@gmail.com>
2016-05-10 21:30:02 -0700

    add timestamp and type to record class

--- addon/-private/Record.js
+++ addon/-private/Record.js
@@ -3,5 +3,18 @@ export default class Record {
     this.key = key;
     this.before = before;
     this.after = after;
+    this.timestamp = (new Date()).toString();
+
+    if(!isUndefined(before) && !isUndefined(after)) {
+      this.type = 'MODIFY';
+    } else if(isUndefined(before)) {
+      this.type = 'ADD';
+    } else {
+      this.type = 'DELETE';
+    }
   }
+}
+
+function isUndefined(value) {
+  return value === undefined;
 }
